b0853f891298364c6363e47314d4a01f4196e4c2
raphaelduarte <raphaelduartessph@gmail.com>
2023-09-30 14:41:15 -0300

    LogradouroController commit Parte 2

---
+++ Psicologia.API/Controllers/Endereco/LogradouroController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Psicologia.Domain.Entities.Endereco;
+using Psicologia.Domain.Repositories.Endereco;
+
+namespace Psicologia.API.Controllers.Endereco;
+
+
+[Route("v1/app/[controller]")]
+[ApiController]
+public class LogradouroController
+{
+    [Route("")]
+    [HttpGet]
+
+    public IEnumerable<Logradouro> GetAll(
+        [FromServices] ILogradouroRepository repository
+        )
+    {
+        return repository.GetAll();
+    }
+}
